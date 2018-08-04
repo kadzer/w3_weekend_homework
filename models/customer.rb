86be43571d7c23225a5ab73c2b78cc0299cb15ab
Kaspars Dzerins <kadzer@gmail.com>
2018-08-04 09:00:01 +0100

    added methods listing films for customer and vice versa, MVP finished

--- models/customer.rb
+++ models/customer.rb
@@ -28,10 +28,21 @@ class Customer
 
 
   def delete()
-      sql = "DELETE FROM customers WHERE id = $1"
-      values = [@id]
-      SqlRunner.run(sql, values)
-    end
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+
+  def films()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    result = films.map { |film| Film.new(film) }
+    return result
+  end
 
 
   def self.all()
--- models/film.rb
+++ models/film.rb
@@ -28,10 +28,21 @@ class Film
 
 
   def delete()
-      sql = "DELETE FROM films WHERE id = $1"
-      values = [@id]
-      SqlRunner.run(sql, values)
-    end
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+
+  def customers()
+    sql = "SELECT customers.* FROM customers
+    INNER JOIN tickets ON customers.id = tickets.customer_id
+    WHERE tickets.film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    result = customers.map { |customer| Customer.new(customer) }
+    return result
+  end
 
 
   def self.all()
